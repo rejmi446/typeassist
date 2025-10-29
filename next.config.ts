d5a2286d50afd2bb1a23129fee9aad174b4f6b95
rejmi446 <adam@rejmann.cz>
2025-10-29 19:53:09 +0100

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000'],
+    },
+  },
+};
+
+export default nextConfig;
